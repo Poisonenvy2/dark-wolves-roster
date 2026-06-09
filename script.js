592c84ee3ed13cf4f687826b67f753047b68970a
Poisonenvy2 <104738891+Poisonenvy2@users.noreply.github.com>
2026-06-09 21:23:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
-const CLIENT_ID = "4232041d22064ebf8d047be3a36f0601";
-const CLIENT_SECRET = "3lqEHSEheOLPsviieV2VdsyNqQ9serSG";
-
-const GUILD_REALM = "tarren-mill";
-const GUILD_NAME = "dark-wolves";
-
 const roster = document.getElementById("roster");
 
-async function getAccessToken() {
+async function loadRoster() {
 
     const response = await fetch(
-        "https://oauth.battle.net/token",
-        {
-            method: "POST",
-            headers: {
-                "Authorization": "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            body: "grant_type=client_credentials"
-        }
+        "https://dark-wolves-api.lowesfamily.workers.dev/"
     );
 
-    const data = await response.json();
+    const guildData = await response.json();
 
-    return data.access_token;
-}
-
-async function getGuildRoster(token) {
-
-    const url =
-        `https://eu.api.blizzard.com/data/wow/guild/${GUILD_REALM}/${GUILD_NAME}/roster?namespace=profile-eu&locale=en_GB&access_token=${token}`;
-
-    const response = await fetch(url);
-
-    return await response.json();
+    renderRoster(guildData.members);
 }
 
 function renderRoster(members) {
@@ -73,23 +49,4 @@ function renderRoster(members) {
     });
 }
 
-async function init() {
-
-    try {
-
-        const token = await getAccessToken();
-
-        const guildData = await getGuildRoster(token);
-
-        renderRoster(guildData.members);
-
-    } catch (err) {
-
-        console.error(err);
-
-        roster.innerHTML =
-            "<p style='padding:20px'>Failed to load roster.</p>";
-    }
-}
-
-init();
+loadRoster();
