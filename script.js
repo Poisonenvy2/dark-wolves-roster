cc1f88433d6af93cdde135afadbd2ec3ef4aa4ae
Poisonenvy2 <104738891+Poisonenvy2@users.noreply.github.com>
2026-06-13 10:53:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -378,3 +378,41 @@ document
     });
 
 loadRoster();
+
+const backToTop =
+    document.getElementById(
+        "backToTop"
+    );
+
+window.addEventListener(
+    "scroll",
+    () => {
+
+        if (
+            window.scrollY > 400
+        ) {
+
+            backToTop.classList.add(
+                "show"
+            );
+
+        } else {
+
+            backToTop.classList.remove(
+                "show"
+            );
+        }
+    }
+);
+
+backToTop.addEventListener(
+    "click",
+    () => {
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+
+    }
+);
